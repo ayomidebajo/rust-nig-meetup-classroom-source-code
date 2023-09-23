d836b83e6ef7a73f1eff10864f3e8969b5a830b2
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-09-23 20:57:43 +0100

    add loop logic for different clients

--- arcs_mutex_locks_client/src/main.rs
+++ arcs_mutex_locks_client/src/main.rs
@@ -1,15 +1,39 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
+// use std::thread::spawn;
 
 fn main() {
     // Connect to the server on localhost:8080
-    let mut stream = TcpStream::connect("127.0.0.1:8080").expect("Failed to connect to server");
+    // let mut stream = TcpStream::connect("127.0.0.1:8080").expect("Failed to connect to server");
 
     // Send data to the server
-    let message = "Hello from the client!";
-    stream
-        .write_all(message.as_bytes())
-        .expect("Failed to write to server");
+    // let message = "Hello from the client another one!";
+    // stream.write_all(message.as_bytes()).expect("Failed to write to server");
+
+    // three different messages from three clients
+    let messages_batch = ["first message from client at 0", "second message from client at 1", "third message from client at 2"];
+
+    // loop logic
+   
+        // spawn(move || {
+            for i in 0..3 {
+                let stream = TcpStream::connect("127.0.0.1:8080").expect("Failed to connect to server");
+                client_handler(messages_batch[i], stream);
+            }
+        // });
+
+    // // Read the server's response
+    // let mut response = String::new();
+    // stream.read_to_string(&mut response).expect("Failed to read response from server");
+
+    // println!("Received from server: {}", response);
+}
+
+fn client_handler(message: &str, mut stream: TcpStream) {
+
+    // Send data to the server
+    // let message = "Hello from the client another one!";
+    stream.write_all(message.as_bytes()).expect("Failed to write to server");
 
     // Read the server's response
     let mut response = String::new();
