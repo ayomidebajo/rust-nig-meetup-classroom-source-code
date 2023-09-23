93fe79ac476f6406d3590e40f29a94139acd38b7
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-09-23 21:07:43 +0100

    remove dead code, add more comments

--- arcs_mutex_locks_client/src/main.rs
+++ arcs_mutex_locks_client/src/main.rs
@@ -1,41 +1,24 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
-// use std::thread::spawn;
 
 fn main() {
-    // Connect to the server on localhost:8080
-    // let mut stream = TcpStream::connect("127.0.0.1:8080").expect("Failed to connect to server");
-
-    // Send data to the server
-    // let message = "Hello from the client another one!";
-    // stream.write_all(message.as_bytes()).expect("Failed to write to server");
-
     // three different messages from three clients
     let messages_batch = ["first message from client at 0", "second message from client at 1", "third message from client at 2"];
 
-    // loop logic
-   
-        // spawn(move || {
             for i in 0..3 {
+                // create a new stream connection for every iteration
                 let stream = TcpStream::connect("127.0.0.1:8080").expect("Failed to connect to server");
+                // parse stream connection and a different message for every iteration
                 client_handler(messages_batch[i], stream);
             }
-        // });
-
-    // // Read the server's response
-    // let mut response = String::new();
-    // stream.read_to_string(&mut response).expect("Failed to read response from server");
-
-    // println!("Received from server: {}", response);
 }
 
 fn client_handler(message: &str, mut stream: TcpStream) {
 
-    // Send data to the server
-    // let message = "Hello from the client another one!";
+    // write to stream with a message (each messages are different)
     stream.write_all(message.as_bytes()).expect("Failed to write to server");
 
-    // Read the server's response
+    // Read the server's response, this logs the shared data which is the overall data/state of the application
     let mut response = String::new();
     stream
         .read_to_string(&mut response)
